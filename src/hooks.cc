43137cb8fe5ab08b2d2a862b9a383f941499e376
Hosam Morad <hosam5.morad3@gmail.com>
2025-06-28 01:24:39 +0300

    __morecore issue

--- src/hooks.cc
+++ src/hooks.cc
@@ -27,6 +27,8 @@ std::mutex g_hook_mmap_mutex;
 //std::mutex g_hook_malloc_mutex;
 bool alloc_request_intercepted = false;
 
+extern "C" void* (*__morecore)(intptr_t);
+
 //void *(*__morecore)(ptrdiff_t) = sbrk;
 //__morecore = sbrk;
 
